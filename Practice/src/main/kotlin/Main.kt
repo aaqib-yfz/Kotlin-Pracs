f0c4e723f9faaa22b77e3126d358a19fe8f0083d
aaqib-yfz <aaqibshaheer2001@gmail.com>
2023-08-19 16:23:27 +0500

    funds

--- Practice/src/main/kotlin/Main.kt
+++ Practice/src/main/kotlin/Main.kt
@@ -1,7 +1,60 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.util.Scanner
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+// Week 01
+fun main(){
+
+////    var name : String = "aaqib"
+////    var age : Int = 10
+////
+//    println( "My name is $name and my age is $age")
+//
+//    var accountType = "free"
+//    if( accountType == "free"){
+//        println(" acc is free")
+//    }
+//    else{
+//        println("Nigga cat")
+//    }
+//    println("Enter Password")
+//    val scanner = Scanner(System.`in`)
+//    val password = scanner.nextLine()
+//    //    var password: String = "adsadasdadsad"
+//
+//    when{
+//        password == "" ->{
+//            println("Password cant be empty")
+//        }
+//        password.length < 7 -> {
+//            println("Password too short")
+//        }
+//        else ->{
+//            println(" nigga cat")
+//        }
+//    }
+
+//    var i = 0 ;
+//    while(i<=10){
+//        println("hello")
+//        i+=1;
+//    }
+//
+//    val letters = arrayListOf("peshawar","quetta","swat","Kandahar")
+//    for(letter in letters){
+//        println("Current city is $letter")
+//    }
+//
+//    for( i in 1.. 5){
+//        println("Number is $i")
+//    }
+//
+//    for( i in 1 until 5){
+//        println("Number is $i")
+//    }
+//
+//    for(i in 1..5 step 2){
+//        println(i)
+//    }
+//
+//    val value = true
+//    println("$value || false")
 }
